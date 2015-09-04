3b2151550cc7a40e625a5976138b946f3ff49b0e
DirkCotton <dirkcotton@gmail.com>
2015-09-04 13:33:22 -0400

    Changed graph labels and title

--- Weighted Probabilities.R
+++ Weighted Probabilities.R
@@ -3,23 +3,23 @@
 faildata <- simulation$failure.year
 deathdata <- lifeSpanMale # simulation$death.years
 rages <- 65:100
-failnums <- rep(36,0)
-deathnums <- failnums
-wtdprob <- failnums
+pRuin <- rep(36,0)
+pAlive <- pRuin
+pAliveandRuined <- pRuin
 
 for (i in 1:36) {
-  failnums[i] <- sum(faildata <= rages[i] )/length(faildata)
-  deathnums[i] <- 1- (sum(lifeSpanMale$V1 <= rages[i])/length(lifeSpanMale$V1))
-  wtdprob[i] <- 1- (failnums[i] * deathnums [i] ) # weighted prob of survival
+  pRuin[i] <- sum(faildata <= rages[i] )/length(faildata)
+  pAlive[i] <- 1- (sum(lifeSpanMale$V1 <= rages[i])/length(lifeSpanMale$V1))
+  pAliveandRuined[i] <- (pRuin[i] * pAlive [i] ) # weighted prob of survival
 }
 
-allData <- data.frame(rages,failnums,deathnums,wtdprob)
+allData <- data.frame(rages,pRuin,pAlive,pAliveandRuined)
 allData.L <- melt(allData,id="rages")
 
 plta <- ggplot(data=allData.L,aes(x=rages, y=value, group=variable,color=variable)) +
   scale_fill_discrete(labels=c("Our Model","Milevsky","Bengen Model")) +
   geom_line() +
-  ggtitle("Weighted Probability of Ruin") +
+  ggtitle("Probability Alive and Ruined by Age") +
   labs(x="Age", y="Probability of Ruin") +
   scale_y_continuous(breaks = seq(0,1,by = .05)) 
 plta
